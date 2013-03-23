aea7c5fd01f22b3eed968df1d489b9b9b39c15c8
shirdrn <shirdrn@gmail.com>
2013-03-23 22:51:25 +0800

    Update.

--- kodz-hadoop/src/main/java/org/shirdrn/kodz/inaction/hadoop/dataset/CountryCodeSet.java
+++ kodz-hadoop/src/main/java/org/shirdrn/kodz/inaction/hadoop/dataset/CountryCodeSet.java
@@ -7,11 +7,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.shirdrn.kodz.inaction.hadoop.utils.FileUtils;
+
 public class CountryCodeSet {
 	
 	private static List<String> CODES = new ArrayList<String>();
 	private static final String WORKSPACE = System.getProperty("user.dir");
-	private static final String SEPARATOR = getPathSeparator();
+	private static final String SEPARATOR = FileUtils.getPathSeparator();
 	private static final String CODE_FILE = WORKSPACE + SEPARATOR
 			+ "src" + SEPARATOR + "main" + SEPARATOR + "resources"
 			+ SEPARATOR + "country_codes";
@@ -33,17 +35,6 @@ public class CountryCodeSet {
 		}
 	}
 	
-	private static String getPathSeparator() {
-		String os = System.getProperty("os.name");
-		String pathSeparator = null;
-		if (os.contains("windows")) {
-			pathSeparator = "\\";
-		} else {
-			pathSeparator = "/";
-		}
-		return pathSeparator;
-	}
-
 	private CountryCodeSet() {
 		super();
 	}
