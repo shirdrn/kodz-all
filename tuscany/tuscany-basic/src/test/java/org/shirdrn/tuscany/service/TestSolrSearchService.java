d0d99e0594b964b96e542fc1aaff3f9b9525784d
Yanjun <shiyanjun@yeah.net>
2013-10-23 18:18:49 +0800

    Add module 'tuscany-basic'.

---
+++ tuscany/tuscany-basic/src/test/java/org/shirdrn/tuscany/service/TestSolrSearchService.java
@@ -0,0 +1,37 @@
+package org.shirdrn.tuscany.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tuscany.sca.Node;
+import org.apache.tuscany.sca.TuscanyRuntime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.oasisopen.sca.NoSuchServiceException;
+
+public class TestSolrSearchService {
+
+	private static final Log LOG = LogFactory.getLog(TestSolrSearchService.class);
+	private Node node;
+	
+	@Before
+	public void initialize() {
+		node = TuscanyRuntime.runComposite("search.composite", "target/classes");
+	}
+	
+	@Test
+	public void testSearch() throws NoSuchServiceException {
+		SolrSearchService solrSearchService = node.getService(SolrSearchService.class, "solrSearchComponent");
+		String collection = "h_d_appraise_gsm";
+		String params = "q=*:*|fq=prov_id:1|indent=true";
+		int start = 0;
+		int rows = 10;
+		String result = solrSearchService.search(collection, params, start, rows);
+		LOG.info("Search result: result=" + result);
+	}
+	
+	@After
+	public void destroy() {
+		node.stop();
+	}
+}
